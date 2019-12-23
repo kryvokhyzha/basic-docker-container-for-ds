b45c34128d1a3da03a0ae35ed45764214c0939c9
Roman Kryvokhyzha <kriwohizha@gmail.com>
2019-12-23 22:40:32 +0200

    Edited travis scripts

--- .travis/dockerHub_deploy.sh
+++ .travis/dockerHub_deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker-compose build --pull
-docker-compose push
-docker logout
+if [ "$TRAVIS_BRANCH" = "master" ] && ["$TRAVIS_PULL_REQUEST" = "false"]; then
+    TAG="latest"
+    
+    docker login -u $DOCKER_USER -p $DOCKER_PASS
+    docker-compose build --pull
+    docker-compose push
+    docker logout
+else
+    TAG="$TRAVIS_BRANCH"
+fi
